aa9774a75d547ea80a302aa113297af659be6fda
UnderscoreNorth <UnderscoreNorth@gmail.com>
2025-12-07 23:48:08 -0500

    eva icons

--- teamcolor.js
+++ teamcolor.js
@@ -1,30 +1,48 @@
 //_North's pixel script
 //var teamList_4cc = ["konata","kagami","tsukasa","miyuki","akira","minami","sojiro","nanako","yui","misao","patricia","yutaka"];
-var teamList_4cc = ["taiga","ami","minori","inko","haruta","kihara","kitamura","sensei","mom","ryuuji","pengin"];
+var teamList_4cc = [
+  'taiga',
+  'ami',
+  'minori',
+  'inko',
+  'haruta',
+  'kihara',
+  'kitamura',
+  'sensei',
+  'mom',
+  'ryuuji',
+  'pengin',
+  'asuka',
+  'rei',
+  'misato',
+  'gendou',
+  'shinji',
+  'penpen',
+];
 
-
-function setTeamList(listOfTeams){
-	var selector = $('#teamcolor');
-	selector.html('<option value="">Chat Icon</option>');
-	listOfTeams.forEach(function(team){
-		selector.append('<option value="' + team + '">/' + team + '/</option>');
-	});
-};
-
-$('#chatwrap').append("<span><select id='teamcolor' style='cursor:default;padding:6px 10px' class='btn btn-sm btn-default'><option value=''>Chat Icon</option></select></span>");
-var TEAMCOLOR = getOrDefault(CHANNEL.name + "_TEAMCOLOR", '');
-setTeamList(teamList_4cc);
-if (TEAMCOLOR){
-	$('#teamcolor').val(TEAMCOLOR);
+function setTeamList(listOfTeams) {
+  var selector = $('#teamcolor');
+  selector.html('<option value="">Chat Icon</option>');
+  listOfTeams.forEach(function (team) {
+    selector.append('<option value="' + team + '">/' + team + '/</option>');
+  });
 }
-$('#teamcolor').change(function(){
-	TEAMCOLOR = $(this).val();
-	setOpt(CHANNEL.name + "_TEAMCOLOR", TEAMCOLOR);
+
+$('#chatwrap').append(
+  "<span><select id='teamcolor' style='cursor:default;padding:6px 10px' class='btn btn-sm btn-default'><option value=''>Chat Icon</option></select></span>"
+);
+var TEAMCOLOR = getOrDefault(CHANNEL.name + '_TEAMCOLOR', '');
+setTeamList(teamList_4cc);
+if (TEAMCOLOR) {
+  $('#teamcolor').val(TEAMCOLOR);
+}
+$('#teamcolor').change(function () {
+  TEAMCOLOR = $(this).val();
+  setOpt(CHANNEL.name + '_TEAMCOLOR', TEAMCOLOR);
 });
 
-
 //Format messages upon page load because they're handled differently and I can't find the function
-$('.teamColorSpan').each(function(){
-	var color = 'team' + $(this).text().replace(new RegExp('Ð','g'),'');
-	$(this).parent().parent().find('.username').addClass(color);
+$('.teamColorSpan').each(function () {
+  var color = 'team' + $(this).text().replace(new RegExp('Ð', 'g'), '');
+  $(this).parent().parent().find('.username').addClass(color);
 });
